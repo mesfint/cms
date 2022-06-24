11ebb6a04be68465b68756d580805c08da85e416
mesfint <mesfin_meng@yahoo.com>
2022-06-24 14:52:21 +0300

    new update

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e56cf720af4177df"; }
+/******/ 		__webpack_require__.h = function() { return "e9435b2f73c4f5cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
